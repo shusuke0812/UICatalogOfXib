a7866f5e06bb7dac06a999a592be04ab9b715ff6
shusuke0812 <shu.ota0812@gmail.com>
2020-07-10 18:47:06 +0900

    [add]tableViewの下地作成

---
+++ UICatalogOfXib/ViewController/TableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewController.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/7/10.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
