67a52cc090b71ac8ba33c94472fc0b12b7393add
shusuke0812 <shu.ota0812@gmail.com>
2020-07-10 23:48:41 +0900

    [add]tableView用のxibを作成

---
+++ UICatalogOfXib/Cells/CommonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommonTableViewCell.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/7/10.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class CommonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
