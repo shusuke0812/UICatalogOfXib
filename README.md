3d349185d91a2db2882cf027b194547021ed8f75
shusuke0812 <shu.ota0812@gmail.com>
2020-07-10 17:54:08 +0900

    初期コミット
    ・プロジェクトファイル作成
    ・README作成
    ・gitignoreファイル作成

---
+++ README.md
@@ -0,0 +1,10 @@
+Xibを使った実装例
+====
+　
+## 開発環境
+- Xcode version 11.3.1
+- pod version 1.9.1
+- MacOS Catalina version 10.15.4
+
+## 概要
+- 準備中  
---
+++ UICatalogOfXib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/7/10.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
