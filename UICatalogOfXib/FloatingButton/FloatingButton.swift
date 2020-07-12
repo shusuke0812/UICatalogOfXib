f9ef42b7dc5b265f2a18683d263bcb30a28be889
shusuke0812 <shu.ota0812@gmail.com>
2020-07-12 16:23:23 +0900

    [add]floatingButtonファイルを追加

---
+++ UICatalogOfXib/FloatingButton/FloatingButton.swift
@@ -0,0 +1,21 @@
+//
+//  FloatingButton.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/7/12.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class FloatingButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
