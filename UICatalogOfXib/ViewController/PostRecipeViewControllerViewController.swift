b39a273a96b22c0148af19dc035d5cb6fad7b5f5
shusuke0812 <shu.ota0812@gmail.com>
2020-07-13 20:28:32 +0900

    [add]レシピ投稿用のstoryboardとviewController追加

---
+++ UICatalogOfXib/ViewController/PostRecipeViewControllerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostRecipeViewControllerViewController.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/7/13.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class PostRecipeViewControllerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
