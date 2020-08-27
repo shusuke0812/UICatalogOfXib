ae9c8b12fe3e28ddd6abdfc2869d2495f67aa879
shusuke0812 <shu.ota0812@gmail.com>
2020-08-27 22:38:49 +0900

    [add]ScrollViewのスクロール位置を取得する関数を追加

--- UICatalogOfXib/ViewController/ScrollViewController.swift
+++ UICatalogOfXib/ViewController/ScrollViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ScrollViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = "Xib Files on Scroll View"
+        self.scrollView.delegate = self
     }
+}
 
+extension ScrollViewController: UIScrollViewDelegate {
+    // スクロール量を取得（スクロール中は常に呼ばれる）
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let contentOffSet: CGPoint = scrollView.contentOffset
+        print(contentOffSet)
+    }
 }
