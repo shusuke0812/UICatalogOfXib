6168d8e1966e7799709ec14dd969838f98bb40ce
shusuke0812 <shu.ota0812@gmail.com>
2020-09-10 23:26:36 +0900

    [add]xibViewとstoryboardを追加

---
+++ UICatalogOfXib/ViewController/StroyboardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StroyboardViewController.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/9/10.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class StroyboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
