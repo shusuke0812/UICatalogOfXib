a6b7dc5cf25716cec40dccac6af7f57ae11a282e
shusuke0812 <shu.ota0812@gmail.com>
2020-09-11 10:47:08 +0900

    [add]xib,storyboard viewへの遷移元のviewControllerを追加

---
+++ UICatalogOfXib/ViewController/TransitionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  transitionViewController.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/9/11.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class TransitionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
