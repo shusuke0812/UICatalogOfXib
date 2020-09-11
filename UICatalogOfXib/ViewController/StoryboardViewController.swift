91519169ddbdc845664afc75cb12ecf25cb4ba3f
shusuke0812 <shu.ota0812@gmail.com>
2020-09-11 11:02:54 +0900

    [rename]スペルミス修正

--- UICatalogOfXib/ViewController/StoryboardViewController.swift
+++ UICatalogOfXib/ViewController/StoryboardViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class StroyboardViewController: UIViewController {
+class StoryboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
