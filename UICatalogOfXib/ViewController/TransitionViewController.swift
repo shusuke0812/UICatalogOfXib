eabe7669f41150ccc42987daacb7a10a5677a7da
shusuke0812 <shu.ota0812@gmail.com>
2020-09-11 11:38:40 +0900

    [add]画面遷移処理を追加

--- UICatalogOfXib/ViewController/TransitionViewController.swift
+++ UICatalogOfXib/ViewController/TransitionViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class TransitionViewController: UIViewController {
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    // MARK: -Action Method
+    // Xibを使ったモーダル遷移
     @IBAction func transitionXibModalButton(_ sender: Any) {
+        let vc: XibViewController = XibViewController(nibName: "XibViewController", bundle: nil)
+        present(vc, animated: true)
     }
+    // Stroyboardを使ったモーダル遷移
     @IBAction func transitionStoryboardButton(_ sender: Any) {
+        let s: UIStoryboard = UIStoryboard(name: "StoryboardViewController", bundle: nil)
+        let vc: StoryboardViewController = s.instantiateInitialViewController() as! StoryboardViewController
+        present(vc, animated: true, completion: nil)
     }
 }
