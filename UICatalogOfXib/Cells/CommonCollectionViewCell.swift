7ea6a53a6525c7d2d1a1d6a9f53a520a2df226b1
shusuke0812 <shu.ota0812@gmail.com>
2020-11-13 14:53:40 +0900

    [fix]写真の上部を角丸にする設定を追加

--- UICatalogOfXib/Cells/CommonCollectionViewCell.swift
+++ UICatalogOfXib/Cells/CommonCollectionViewCell.swift
@@ -9,8 +9,19 @@
 import UIKit
 
 class CommonCollectionViewCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        self.setUI()
+    }
+}
+// MARK: - Initialized Method
+extension CommonCollectionViewCell {
+    private func setUI() {
+        // 写真の左上・右上の角を丸くする
+        self.imageView.layer.cornerRadius = 15
+        self.imageView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
 }
