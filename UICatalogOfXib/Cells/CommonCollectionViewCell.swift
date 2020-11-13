f61b1111b286676413e5d9d1796af8852a06f843
shusuke0812 <shu.ota0812@gmail.com>
2020-11-13 14:29:56 +0900

    [add]CollectionViewファイルを追加

---
+++ UICatalogOfXib/Cells/CommonCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CommonCollectionViewCell.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/11/13.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class CommonCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
---
+++ UICatalogOfXib/ViewController/CollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewController.swift
+//  UICatalogOfXib
+//
+//  Created by Shusuke Ota on 2020/11/13.
+//  Copyright © 2020 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
